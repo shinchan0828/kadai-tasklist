a3f9e9e0150fb14c2a526a8cc1c3bc70b83a83d6
marcos.test <marcos.test@gmail.com>
2017-06-14 15:32:42 +0000

    polished kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,21 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
   
   def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new  
   end
   
   def create
   end
   
   def edit
+    @task = Task.find(params[:id])
   end
   
   def update
